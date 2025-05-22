001acf1c4c8ac3312032cd27d02b84a7f50e7e5c
Bziyue <954164207@qq.com>
2025-05-22 23:26:20 +0800

    洛谷p1440

---
+++ src/基础数据结构/队列/洛谷P1440求m区间内的最小值单调队列/洛谷P1440求m区间内的最小值单调队列.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 2e6+5;
+int arr[N];
+deque<int> dq;
+int main()
+{
+    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+    int n,m;cin >> n >> m;
+    for(int i = 1; i <= n; i++) cin>>arr[i];
+    for(int i = 1; i <= n; i++)
+    {
+
+        //去除超过窗口范围的元素
+        while(!dq.empty() && dq.front() < i - m) dq.pop_front();
+
+        if(dq.empty()) cout<<0<<endl;
+        else
+        {
+            cout<<arr[dq.front()]<<'\n';
+        }
+
+        while(!dq.empty() && arr[dq.back()] >= arr[i]) dq.pop_back();
+
+        dq.push_back(i);
+    }
+
+
+    return 0;
+}
