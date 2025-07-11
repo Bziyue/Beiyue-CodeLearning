60f7c4c665c13a93c147dbbe6d4b77804eef6f51
Bziyue <954164207@qq.com>
2025-07-11 13:20:25 +0800

    力扣-不同路径数

---
+++ src/动态规划/不同路径/力扣-不同路径-降低空间复杂度.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        //空间复杂度O(n)做法
+        vector<int> dp(n,1);
+
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 1; j < n; j++)
+            {
+                dp[j] = dp[j] + dp[j-1];
+            }
+        }
+
+        return dp[n-1];
+    }
+};
